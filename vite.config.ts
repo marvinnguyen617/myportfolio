86228e5d37296c1f94cfb1e8698631dc18eb6c52
marvinnguyen617 <marvinnguyen617@gmail.com>
2026-03-22 21:09:06 -0700

    removed unused variables

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/marvintuanminhnguyen.github.io/',
+  base: '/myportfolio/',
   plugins: [
     react({
       babel: {
