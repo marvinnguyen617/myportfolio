1c744ef6e65652205f3c30cf444c94220ebeb959
marvinnguyen617 <marvinnguyen617@gmail.com>
2026-03-24 16:02:46 -0700

    added contact page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import { Home } from './Pages/home'
 import { About } from './Pages/about'
 import { Projects } from './Pages/projects'
+import { Contact } from './Pages/contact'
 import { Layout } from './Components/layout'
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
         </Route>
       </Routes>
     </Router>
---
+++ src/Pages/contact.tsx
@@ -0,0 +1,9 @@
+export function Contact() {
+    return (
+    <>
+        <h1>Contact Me</h1>
+        <h2>Email:</h2>
+        <h3>marvintn@uci.edu</h3>
+    </>
+    )
+}
